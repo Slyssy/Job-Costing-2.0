ee5e5053f00f546f485b349906e6d3dfdd237b8f
Stephen <stephen.lyssy@gmail.com>
2021-01-02 18:11:37 -0600

    Just commented and uncommented code so that features that aren't working won't stop the rest of the features from running.

--- static/d3Map.js
+++ static/d3Map.js
@@ -1,17 +1,17 @@
-let margin = {top: 70, right: 20, bottom: 70, left: 125};
+// let margin = {top: 70, right: 20, bottom: 70, left: 125};
 
-let width = 1135 - margin.left - margin.right;
-let height = 600 - margin.top - margin.bottom;
+// let width = 1135 - margin.left - margin.right;
+// let height = 600 - margin.top - margin.bottom;
 
-let path = d3.geo.path();
+// let path = d3.geo.path();
 
-let svg = d3.select('#project-map').append('svg').attr({width:width, height:height});
+// let svg = d3.select('#project-map').append('svg').attr({width:width, height:height});
 
-d3.json9('cb_2018_us_state_500k.geojson', function(json){
-    svg.selectAll('path')
-    .data(json.features)
-    .enter()
-    .append('path')
-    .attr('d', path)
-    .attr('fill', '#666666')
-});
+// d3.json9('cb_2018_us_state_500k.geojson', function(json){
+//     svg.selectAll('path')
+//     .data(json.features)
+//     .enter()
+//     .append('path')
+//     .attr('d', path)
+//     .attr('fill', '#666666')
+// });
