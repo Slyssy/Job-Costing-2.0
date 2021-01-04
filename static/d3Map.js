09d898f9b02f4a7488d24c508000a9cb7174bac7
Stephen <stephen.lyssy@gmail.com>
2021-01-03 22:41:15 -0600

    reconfigured files. Still not working.

--- static/d3Map.js
+++ static/d3Map.js
@@ -1,13 +1,16 @@
-// let margin = {top: 70, right: 20, bottom: 70, left: 125};
+// const { select } = require("d3");
 
-// let width = 1135 - margin.left - margin.right;
-// let height = 600 - margin.top - margin.bottom;
+const marginMap = {top: 70, right: 20, bottom: 70, left: 125};
 
-// let path = d3.geo.path();
+const widthMap = 1135 - marginMap.left - marginMap.right;
+const heightMap = 600 - marginMap.top - marginMap.bottom;
 
-// let svg = d3.select('#project-map').append('svg').attr({width:width, height:height});
+const svgMap = d3.select('#project-map')
+// let path = d3.geoPath();
 
-// d3.json9('cb_2018_us_state_500k.geojson', function(json){
+// d3.select('#project-map').append('svg').attr({width:width, height:height});
+
+// d3.json('us_map.json', function(json){
 //     svg.selectAll('path')
 //     .data(json.features)
 //     .enter()
@@ -15,3 +18,19 @@
 //     .attr('d', path)
 //     .attr('fill', '#666666')
 // });
+
+// let url = "https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/us.json"
+const projection = d3.geoMercator();
+const pathGenerator = d3.geoPath().projection(projection);
+
+d3.json("https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/us.json")
+    .then(data => {
+        const states = topojson.feature(data, data.objects.states);
+        console.log(data);
+        console.log(states)
+
+        const paths = svgMap.selectAll('path')
+            .data(states.geometries);
+        paths.enter().append('path')
+            .attr('d', d => pathGenerator(d));
+    })
