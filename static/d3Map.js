944d6480f7b205eed4b4a8d8904820a1ae53606f
Stephen <stephen.lyssy@gmail.com>
2021-01-04 23:05:04 -0600

    Made some progress on the project map.I was able to get the map to load with the state boudaries. I also added some formatting for hover effects and got the pan and zoom to work.

--- static/d3Map.js
+++ static/d3Map.js
@@ -1,36 +1,49 @@
 // const { select } = require("d3");
 
-const marginMap = {top: 70, right: 20, bottom: 70, left: 125};
-
-const widthMap = 1135 - marginMap.left - marginMap.right;
-const heightMap = 600 - marginMap.top - marginMap.bottom;
+const marginMap = {top: 0, right: 0, bottom: 0, left: 0};
+const widthMap = 1135 - margin.left - margin.right;
+const heightMap = 600 - margin.top - margin.bottom;
 
 const svgMap = d3.select('#project-map')
-// let path = d3.geoPath();
+    .append('svg')
+    .attr('height', height + margin.top + margin.bottom)
+    .attr('width', width + margin.left + margin.right)
+    .append('g')
+    .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
-// d3.select('#project-map').append('svg').attr({width:width, height:height});
-
-// d3.json('us_map.json', function(json){
-//     svg.selectAll('path')
-//     .data(json.features)
-//     .enter()
-//     .append('path')
-//     .attr('d', path)
-//     .attr('fill', '#666666')
-// });
-
-// let url = "https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/us.json"
-const projection = d3.geoMercator();
+const projection = d3.geoAlbersUsa();
 const pathGenerator = d3.geoPath().projection(projection);
 
-d3.json("https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/us.json")
+const g = svgMap.append('g');
+
+svgMap.call(d3.zoom().on("zoom", () => {
+    g.attr("transform", d3.event.transform);
+}))
+
+d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json")
     .then(data => {
         const states = topojson.feature(data, data.objects.states);
-        console.log(data);
-        console.log(states)
+        // console.log(data);
+        // console.log(states);
+        g.selectAll('path').data(states.features)
+        .enter().append('path')
+        .attr('class', 'state')
+        .attr('d', pathGenerator)
+        .append('title')
+        .text('State Name')
 
-        const paths = svgMap.selectAll('path')
-            .data(states.geometries);
-        paths.enter().append('path')
-            .attr('d', d => pathGenerator(d));
+        // const paths = g.selectAll('path')
+        //     .data(states.features);
+
+        // paths.enter().append('path')
+        //     .attr('class', 'state')
+        //     .attr('d', d => pathGenerator(d))
+        //     .append('title')
+        //         .text('Hello');
+            
     })
+
+    
+
+    
+
