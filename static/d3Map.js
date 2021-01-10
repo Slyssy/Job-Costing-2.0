25d13b62afd6283a95e8e7a749272622dd50eb28
Stephen <stephen.lyssy@gmail.com>
2021-01-10 17:26:34 -0600

    Added city markers and names to the map.

--- static/d3Map.js
+++ static/d3Map.js
@@ -19,7 +19,8 @@ const pathGenerator = d3.geoPath().projection(projection);
 
 const zoomG = svgMap.append('g');
 const mapG = zoomG.append('g');
-const circlesG = zoomG.append('g');
+const cityG = zoomG.append('g');
+const projectG = zoomG.append('g');
    
 d3.json("https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/us.json")
     .then(data => {
@@ -40,67 +41,40 @@ d3.json("https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba16920754
         // .append('title')
         // .text('State Name')      
 });
-// // Load in Cities Data
-// d3.tsv('static/uscities.tsv')
-//     .then(data => {
-//         console.log(data)
-//         circlesG.selectAll('circle').raise()
-//             .data(data)
-//             .enter().append('circle')
-//             .attr('class', "cities")
-//             .attr('r',  d => {
-//                 return Math.sqrt(+d.population/ widthMap *0.005);
-//             })
-//             .attr('cx', d => {
-//                 return projection([d.lng, d.lat])[0];
-//             })
-//             .attr('cy', d => {
-//                 return projection([d.lng, d.lat])[1];
-//             })
+// Load in Cities Data
+d3.tsv('static/uscities.tsv')
+    .then(data => {
+        console.log(data)
+        cityG.selectAll('circle').raise()
+            .data(data)
+            .enter().append('circle')
+            .attr('class', "cities")
+            .attr('r',  '1')
+            .attr('cx', d => {
+                return projection([d.lng, d.lat])[0];
+            })
+            .attr('cy', d => {
+                return projection([d.lng, d.lat])[1];
+            })
      
-        
+// Adding names to cities
+            cityG.selectAll("text")
+                .data(data)
+                .enter()
+                .append("text") // append text
+                .attr('class', 'cityName')
+                 .attr("x", function(d) {
+                 return projection([d.lng, d.lat])[0];
+                })
+                .attr("y", function(d) {
+                return projection([d.lng, d.lat])[1];
+                })
+                .attr("dy", 8) // set y position of bottom of text
+                .attr("text-anchor", "end") // set anchor y justification
+                .text(function(d) {return d.city;}); // define the text to display        
             
-//     // Adding Tooltip Behavior
-
-//     .on('mouseover', function(d)  {
-//         d3.select(this).style('fill', '#a834eb')
-//         d3.select("#name").text(" " + d.city)
-//         d3.select("#population").text(valueFormat(" " + d.population))
-//         d3.select("#latitude").text(" " + d.lat)
-//         d3.select("#longitude").text(" " + d.lng)
-
-//         //Position the tooltip <div> and set its content
-//         let x = d3.event.pageX;
-//         let y = d3.event.pageY - 40;
-
-//         // //Populate city name in tooltip
-//         // d3.select('#tooltip .name')
-//         //     .text(d.city);
-
-//         //Populate city population value into tooltip
-//         // d3.select('#tooltip .value')
-//         //     .text("population:" + valueFormat(d.population));
-        
-//         //Position tooltip and make it visible
-//         d3.select('#tooltip')
-//             .style('left', x +'px')
-//             .style('top', y + 'px')
-//             .style('opacity', 1)
-//     })
-//     .on('mouseout', function() {
-//         d3.select(this).style('fill', '#eb2828');        
-        
-//         //Hide the tooltip
-//         d3.select('#tooltip')
-//             .style('opacity', '0');
-//     });
-
-//     //Creating pan and zoom feature
-//     zoomG.call(d3.zoom().on("zoom", () => {
-//         zoomG.attr("transform", d3.event.transform);
-//     }))
             
-// })
+})
 
 console.log(projectArray)
 //Parsing and formatting data to be used for map points
@@ -113,10 +87,10 @@ function map(mapData) {
     // d3.tsv('static/uscities.tsv')
     //     .then(data => {
             console.log(mapData)
-            circlesG.selectAll('circle').raise()
+            projectG.selectAll('circle').raise()
                 .data(mapData)
                 .enter().append('circle')
-                .attr('class', "cities")
+                .attr('class', "projects")
                 .attr('r',  d => {
                     return Math.sqrt(d.fin_act_revenue/ widthMap *0.05);
                 })
@@ -141,14 +115,6 @@ function map(mapData) {
             //Position the tooltip <div> and set its content
             let x = d3.event.pageX;
             let y = d3.event.pageY - 40;
-    
-            // //Populate city name in tooltip
-            // d3.select('#tooltip .name')
-            //     .text(d.city);
-    
-            //Populate city population value into tooltip
-            // d3.select('#tooltip .value')
-            //     .text("population:" + valueFormat(d.population));
             
             //Position tooltip and make it visible
             d3.select('#tooltip')
