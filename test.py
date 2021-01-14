fc22abbda3e60c1756d5ffa9051c71d76f7cae45
jgandre <67715755+jgandre@users.noreply.github.com>
2021-01-14 11:32:20 -0600

    test.py has tupple conversion

--- test.py
+++ test.py
@@ -11,6 +11,8 @@ from geopy.exc import GeocoderTimedOut
 from crud import *
 import hashlib
 from passlib.hash import sha256_crypt
+import functools
+import operator
 
 ctx = ssl.create_default_context(cafile=certifi.where())
 geopy.geocoders.options.default_ssl_context = ctx
@@ -50,12 +52,35 @@ app = Flask(__name__)
 # def index():
 #     return render_template("index.html")
 
+# log_in_name = request.form['employee_name']
 log_in_name = "bob"
-cur.execute('SELECT password from users WHERE log_in=%s;' [log_in_name])
+cur.execute('SELECT password from users WHERE log_in=%s;', [log_in_name])
 # cur.execute('SELECT log_in, password from users')
 rows = cur.fetchall()
 
+#creates [('password',)]
 print(rows)
+#creates ('password',)
+print(rows[0])
+
+tup= rows[0] 
+
+strpass = functools.reduce(operator.add,(tup))
+print(strpass)
+
+# def convertTuple(tup):
+#     str = functools.reduce(operator.add,(tup))
+#     print (str)
+
+if "passbob20" == strpass:
+    print ("it matches")
+else:
+    print("does not match")
+
+if "passbob20" == strpass:
+    print ("it matches")
+else:
+    print("does not match")
 
 # for r in rows:
 #     password = r[0]
