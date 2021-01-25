7d3b322ea5ea8ce50e8b9debc984214bcf85d21f
Karen Kitchens <karen.kitchens@gmail.com>
2021-01-24 21:02:20 -0600

    Adding expenses and math to the dashboard

--- static/app.js
+++ static/app.js
@@ -53,29 +53,50 @@ function buildTable(data){
   let table = document.getElementById('dashboardTable')
   table.innerHTML = ''
   for (let i = 0; i < data.length; i++){
-
     let hours_over = false
+    let labor_over = false
+    let material_over = false
+    let misc_over = false
+    let subcontractor_over = false
+    
     let flag = '';
     if (data[i].fin_est_labor_hours - data[i].fin_act_labor_hours < 0 ) {
       hours_over = true;
       flag = 'flag';
     }
     
+    if (parseFloat(data[i].fin_est_labor_expense.replace(/,/g, '')) - parseFloat(data[i].fin_act_labor_expense.replace(/,/g, '')) < 0 ) {
+      labor_over = true;
+      flag = 'flag';
+    }
+    if (parseFloat(data[i].fin_est_material_expense.replace(/,/g, '')) - parseFloat(data[i].fin_act_material_expense.replace(/,/g, '')) < 0 ) {
+      material_over = true;
+      flag = 'flag';
+    }
+    if (parseFloat(data[i].fin_est_miscellaneous_expense.replace(/,/g, '')) - parseFloat(data[i].fin_act_miscellaneous_expense.replace(/,/g, '')) < 0 ) {
+      misc_over = true;
+      flag = 'flag';
+    }
+    if (parseFloat(data[i].fin_est_subcontractor_expense.replace(/,/g, '')) - parseFloat(data[i].fin_act_subcontractor_expense.replace(/,/g, '')) < 0 ) {
+      subcontractor_over = true;
+      flag = 'flag';
+    }
+    
     let  row = `<tr>
-                             <td><a class="${flag}" href="/search?project_id=${data[i].id}">${data[i].project_name}</a></td>
+                             <td><a href="/search?project_id=${data[i].id}">${data[i].project_name}</a></td>
                               <td class="revenue">$${data[i].fin_act_revenue}</td>
                               <td class="labor-hours border-secondary border-left">${data[i].fin_est_labor_hours}</td>
                               <td class="labor-hours ${hours_over}">${data[i].fin_act_labor_hours}</td>
-                              <td class="labor border-secondary border-left">$${data[i].fin_est_labor_expense}</td>
-                              <td class="labor">$${data[i].fin_act_labor_expense}</td>
+                              <td class="labor border-secondary border-left">$${data[i].fin_est_labor_expense} </td>
+                              <td class="labor ${labor_over}">$${data[i].fin_act_labor_expense}</td>
                               <td class="material border-secondary border-left">$${data[i].fin_est_material_expense}</td>
-                              <td class="material">Coming Soon</td>
+                              <td class="material ${material_over}">$${data[i].act_material_expense}</td>
                               <td class="misc border-secondary border-left">$${data[i].fin_est_miscellaneous_expense}</td>
-                              <td class="misc">Coming Soon</td>
+                              <td class="misc ${misc_over}">$${data[i].fin_act_miscellaneous_expense}</td>
                               <td class="overhead border-secondary border-left">$${data[i].fin_est_overhead_expense}</td>
-                              <td class="overhead">Coming Soon</td>
+                              <td class="overhead">$${data[i].fin_act_overhead_expense}</td>
                               <td class="subcontractor border-secondary border-left">$${data[i].fin_est_subcontractor_expense}</td>
-                              <td class="subcontractor">Coming Soon</td>
+                              <td class="subcontractor ${subcontractor_over}">$${data[i].fin_act_subcontractor_expense}</td>
                               <td class="start-date">${data[i].act_start_date}</td>
                           </tr>`
        table.innerHTML += row   
